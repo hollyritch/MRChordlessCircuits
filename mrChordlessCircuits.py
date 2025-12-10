a8e9e8b6412ae34a3d05c8499edfe194f26b99be
hollyritch <rgolnik@posteo.de>
2025-12-10 08:29:42 -0300

    .

--- mrChordlessCircuits.py
+++ mrChordlessCircuits.py
@@ -5,6 +5,9 @@ import time
 import numpy as np
 from matplotlib import pyplot as plt
 from tqdm import tqdm
+import pickle
+import sys, os
+from copy import deepcopy
 
 class _NeighborhoodCache(dict):
     """Very lightweight graph wrapper which caches neighborhoods as list.
@@ -20,9 +23,11 @@ class _NeighborhoodCache(dict):
     def __missing__(self, v):
         Gv = self[v] = set(self.G[v])
         return Gv
+#############################
+#############################
 
 
-def chordless_cycle_search_Species(F, B, path, length_bound, G):
+def chordless_cycle_search_Species(F, B, path:list, length_bound:int, metabolites):
     fwBlocked = defaultdict(int)
     bwBlocked = defaultdict(int)
     target = path[0]
@@ -38,32 +43,36 @@ def chordless_cycle_search_Species(F, B, path, length_bound, G):
             for m in Ba:
                 bwBlocked[m] 
     stack = [iter(F[path[2]])]
+    if fwBlocked[path[1]]>1:
+        return
     while stack:
         nbrs = stack[-1]
         for x in nbrs:
             if x==target:
                 continue
-            if G.nodes[x]["Type"] == "Species":
+            if x in metabolites:
                 proceed = (bwBlocked[x]==0 and (length_bound is None or len(path) < length_bound))
             else:
                 proceed = (fwBlocked[x] == 1 and (length_bound is None or len(path) < length_bound))
             if not proceed:
                 continue
             Fx = F[x]
-            Bx = B[x]
+            
             if target in Fx:                        # x is a reaction, target a metabolite
-                if bwBlocked[target]==0:
-                    yield path + [x]                    # yield the path
+                if bwBlocked[target]==0:                    
+                    yield path + [x]
+                    Bx = B[x]
                     for m in Bx:
                         bwBlocked[m] += 1
                     path.append(x)
                     stack.append(iter(Fx))
                     break
             else:
+                Bx = B[x]
                 if target in Bx:                    # since target is a metabolite, then x is a reaction
                     continue                        # we can probably remove this check, should have been done before
                 else:        
-                    if G.nodes[x]["Type"]=="Species":
+                    if x in metabolites:
                         if second in Fx and fwBlocked[second]==1:
                             continue
                         for r in Fx:
@@ -77,7 +86,7 @@ def chordless_cycle_search_Species(F, B, path, length_bound, G):
         else:                                       # Take off 
             stack.pop() 
             z = path.pop()
-            if G.nodes[z]["Type"]=="Species":
+            if z in metabolites:
                 Fz=F[z]
                 for y in Fz:
                     fwBlocked[y] -= 1
@@ -89,73 +98,13 @@ def chordless_cycle_search_Species(F, B, path, length_bound, G):
 #############################
 
 
-def chordless_cycle_search_Reaction(F, B, path, length_bound, G):
-    ## Reaction is the first node
-    fwBlocked = defaultdict(int)                                    # will contain only reactions
-    bwBlocked = defaultdict(int)                                    # will contain only metabolites‚
-    target = path[0]                                                # Target is a reaction
-    for i in range(1,len(path)):                                      # Initializing
-        a = path[i]
-        if i%2==0:                                                  # Reaction case 
-            Ba=B[a]
-            for m in Ba:
-                bwBlocked[m] +=1                                    # Block m from being visited (only unvisited metabolites can be visited)
-        else:      
-            Fa=F[a]                                                 # Species case
-            for r in Fa:                                            # Block reactions from being visited twice
-                fwBlocked[r] += 1
-    stack = [iter(F[path[2]])]
-    while stack:                                                    # Now the real loop
-        nbrs = stack[-1]
-        for x in nbrs:
-            if G.nodes[x]["Type"] == "Species":
-                proceed = (bwBlocked[x]==0 and (length_bound is None or len(path) < length_bound))
-            else:
-                proceed = (fwBlocked[x] == 1 and (length_bound is None or len(path) < length_bound))
-            if not proceed:
-                continue
-            Fx = F[x]
-            if target in Fx:                            # Remember: x is a metabolite
-                if sum(1 for y in Fx if y in path)==1 and fwBlocked[target]==0:
-                    yield path + [x]                    # yield the path and stop.....you can' go any further, otherwise there would be non MR-chordfree cycles
-            else:                                   
-                if G.nodes[x]["Type"]=="Species":   # Block forward in the case of a metabolite
-                    for y in Fx:
-                        fwBlocked[y] += 1
-                else:                               
-                    Bx = B[x]
-                    for y in Bx:                    # Block backward in case of a reaction
-                        bwBlocked[y] += 1
-                path.append(x)                      # Either way, append x to the path add all forward oriented vertices 
-                                                    # to the stack
-                stack.append(iter(Fx))
-                break
-        else:                                       # Take off 
-            stack.pop() 
-            z = path.pop()
-            if G.nodes[z]["Type"]=="Species":
-                Fz=F[z]
-                for y in Fz:
-                    fwBlocked[y] -= 1
-            else:
-                Bz = B[z]
-                for y in Bz:
-                    bwBlocked[y] -= 1
-#############################
-#############################
-
-
-def chordless_cycle_search(F, B, path, length_bound, G):
-    if G.nodes[path[0]]["Type"]=="Species":
-        yield from chordless_cycle_search_Species(F, B, path, length_bound, G)
-    else:
-        yield from chordless_cycle_search_Reaction(F, B, path, length_bound, G)
-#############################
-#############################
-
-
-def findAllMRChordlessCyclesList(F, bound):
+def findAllMRChordlessCyclesList(F, reactions:set, metabolites:set, bound:int):
     B = F.reverse(copy=True)
+    for u in metabolites:
+        Fu = F.successors(u)
+        digons = [[u, v] for v in Fu if F.has_edge(v, u)]
+        yield from digons
+        
     def stems(C, v):
         for u, w in product(C.pred[v], C.succ[v]):
             yield [u, v, w]
@@ -163,37 +112,48 @@ def findAllMRChordlessCyclesList(F, bound):
     components = [c for c in nx.strongly_connected_components(F) if len(c) > 3]
     while components:
         c = components.pop()
-        v = next(iter(c))   
-        Fc = F.subgraph(c)
-        Fcc = Bcc = None
-        for S in stems(Fc, v):
-            if Fcc is None:
-                Fcc = nx.algorithms.cycles._NeighborhoodCache(Fc)
-                Bcc = nx.algorithms.cycles._NeighborhoodCache(B.subgraph(c))
-            yield from chordless_cycle_search(Fcc, Bcc, S, bound, F)
-        components.extend(c for c in nx.strongly_connected_components(F.subgraph(c - {v})) if len(c) > 3)
+        rx = reactions & c
+        if rx:
+            v = next(iter(rx))
+            Fc = F.subgraph(c)
+            Fcc = Bcc = None
+            for S in stems(Fc, v):
+                if Fcc is None:
+                    Fcc = nx.algorithms.cycles._NeighborhoodCache(Fc)
+                    Bcc = nx.algorithms.cycles._NeighborhoodCache(B.subgraph(c))
+                yield from chordless_cycle_search_Species(Fcc, Bcc, S, bound, metabolites)
+                #yield from chordless_cycle_search(Fcc, Bcc, S, bound, F, maxIndex)
+            components.extend(c for c in nx.strongly_connected_components(F.subgraph(c - {v})) if len(c) > 3)
 #############################
 #############################
 
 
-def findAllMRChordlessCyclesSet(F, bound):
+def findAllMRChordlessCyclesSet(F, reactions:set, metabolites:set, bound:int):
     B = F.reverse(copy=True)
+    for u in metabolites:
+        Fu = F.successors(u)
+        digons = [[u, v] for v in Fu if F.has_edge(v, u)]
+        yield from digons
+        
     def stems(C, v):
         for u, w in product(C.pred[v], C.succ[v]):
             yield [u, v, w]
 
-    components = [c for c in nx.strongly_connected_components(F) if len(c) > 3]
+    separate = nx.strongly_connected_components
+    components = [c for c in separate(F) if len(c) > 3]
     while components:
         c = components.pop()
-        v = next(iter(c))   
-        Fc = F.subgraph(c)
-        Fcc = Bcc = None
-        for S in stems(Fc, v):
-            if Fcc is None:
-                Fcc = _NeighborhoodCache(Fc)
-                Bcc = _NeighborhoodCache(B.subgraph(c))
-            yield from chordless_cycle_search(Fcc, Bcc, S, bound, F)
-        components.extend(c for c in nx.strongly_connected_components(F.subgraph(c - {v})) if len(c) > 3)
+        rx = reactions & c
+        if rx:
+            v = next(iter(rx))
+            Fc = F.subgraph(c)
+            Fcc = Bcc = None
+            for S in stems(Fc, v):
+                if Fcc is None:
+                    Fcc = _NeighborhoodCache(Fc)
+                    Bcc = _NeighborhoodCache(B.subgraph(c))
+                yield from chordless_cycle_search_Species(Fcc, Bcc, S, bound, metabolites)
+            components.extend(c for c in separate(F.subgraph(c - {v})) if len(c) > 3)
 #############################
 #############################
 
@@ -221,43 +181,15 @@ def findNodeSpecificMRChordlessCycles(F, x, bound):
 #############################
 
 
-def callFindMRChordlessCircuits(H:nx.DiGraph, type:str, bound:int):
-    for comp in nx.strongly_connected_components(H):
-        F = H.subgraph(comp)
-        X, Y = nx.algorithms.bipartite.sets(F, top_nodes=None)
-        if len(X)<len(Y):
-            M=X
-            R=Y
-        else:
-            M=Y
-            R=X
-        for m in M:
-            F.nodes[m]["Type"]="Species"
-        for r in R:
-            F.nodes[r]["Type"]="Reaction"
-        for n in F.nodes():
-            if "Type" not in F.nodes[n]:
-                print(n, n in X, n in R, F.nodes[n])
-                input()
-        if type == "List":
-            for c in findAllMRChordlessCyclesList(F, bound):
-                continue
-        if type == "Set":
-            for c in findAllMRChordlessCyclesSet(F, bound):
-                continue
-#############################
-#############################
-
-
-def checkCycle(c, H):
+def checkCycle(c, K):
     chordFree = True
     for v in c:
-        if H.nodes[v]["Type"]=="Reaction":            
-            if sum(1 for u in c if (u,v) in H.edges())>1:
+        if K.nodes[v]["Type"]=="Reaction":            
+            if sum(1 for u in c if H.has_edge(u,v))>1:
                 chordFree=False
                 break
-        if H.nodes[v]["Type"]=="Species":            
-            if sum(1 for u in c if (v,u) in H.edges())>1:
+        if K.nodes[v]["Type"]=="Species":            
+            if sum(1 for u in c if H.has_edge(v,u))>1:
                 chordFree=False
                 break
     return chordFree
@@ -265,66 +197,123 @@ def checkCycle(c, H):
 #############################
 
 
-def plotResults(m, p, keys, timeDict):
+def plotResults(minNoNodes:int, maxNoNodes:int, p:float, keys:set, path:str, timeDict:dict):
     x = np.arange(len(keys))  # the label locations
     width = 0.1  # the width of the bars
     multiplier = 0
-
+    newTimeDict = {}
+    i=0
+    for counter in keys:
+        setList= timeDict["MR-chordlessSet"][counter]
+        listList= timeDict["MR-chordlessList"][counter]
+        johnsonList= timeDict["Johnson"][counter]
+        newTimeDict.setdefault("MR-chordlessSet", []).append(sum(setList)/len(setList))
+        newTimeDict.setdefault("MR-chordlessList", []).append(sum(listList)/len(listList))
+        newTimeDict.setdefault("Johnson", []).append(sum(johnsonList)/len(johnsonList))
+    
     fig, ax = plt.subplots(layout='constrained')
     fig.set_figheight(10)
     fig.set_figwidth(20)
-    for attribute, measurement in timeDict.items():
+    for attribute, measurement in newTimeDict.items():
         offset = width * multiplier
         rects = ax.bar(x+0.1 + offset, measurement, width, label=attribute)
         multiplier += 1
     n = 28
     ax.set_ylabel(ylabel="Time", fontsize=n)
+    ax.set_title(label="Random graphs of max. size " +str(maxNoNodes), fontsize=n+4)
     ax.set_yscale("log")
-    ax.set_xlabel(xlabel="Number of nodes", fontsize=n)
-    ax.set_xticks(x + width, keys, fontsize=n)
-    ax.legend(loc='upper left', ncols=3, fontsize=n)
+    ax.set_xlabel(xlabel="Number of MR-chordless cycles", fontsize=n)
+    ax.set_xticks(x + width, keys, fontsize=n, rotation=45)
+    ax.legend(loc='upper left', ncols=1, fontsize=n)
     plt.yticks(fontsize=n)
-    plt.savefig("./Benchmarking/MRChordlessVsJohnson_"+str(p)+".png")
+    plt.savefig(path + "/MRChordlessVsJohnson_"+str(p)+"min_" + str(minNoNodes) + "max_" +str(maxNoNodes)+".png")
+    plt.close()
 #############################
 #############################
 
-x = 10
-m = 10
-bound=20
-for k in tqdm(range(1, 101, 1), leave=False, total=20):
-    p = k/100
-    print(p)
-    timeDict = {}
-    keys = []
-    list = range(m)
-    for i in tqdm(range(len(list)), leave = False, total=len(list)):
-        if i==0:
-            continue
-        n=i*x
-        H=nx.algorithms.bipartite.random_graph(n=n, m=n, p=p, directed=True)
-        
-        timeStamp = time.time()
-        print("Starting to compute MR-Chordless cycles with set")
-        callFindMRChordlessCircuits(H, "Set", bound)
-        setTime = time.time()-timeStamp
-        
-        timeStamp = time.time()
-        print("Starting to compute MR-Chordless cycles with list")
-        callFindMRChordlessCircuits(H, "List", bound)
-        listTime = time.time()-timeStamp
-        
-        timeStamp = time.time()
-        print("Starting to compute all cycles")
-        for c in nx.simple_cycles(H, length_bound=bound):
-            checkCycle(c, H)
-        johnsonTime = time.time()-timeStamp
-        if n==1*x:
-            timeDict["MR-chordlessSet"]=[setTime]
-            timeDict["MR-chordlessList"]=[listTime]
-            timeDict["Johnson"]=[johnsonTime]
-        else:
-            timeDict["MR-chordlessSet"].append(setTime)
-            timeDict["MR-chordlessList"].append(listTime)
-            timeDict["Johnson"].append(johnsonTime)
-        keys.append(n)
-    plotResults(2*n, p, keys, timeDict)
+l = 10
+maxSize = 6
+
+a = float(sys.argv[1])
+b = float(sys.argv[2])
+direc= "./Benchmarking/"+str(a)+"to"+str(b)
+if not os.path.exists(direc):
+    os.makedirs(direc)
+
+bound=None
+timeDict = {}
+myList = range(maxSize)
+for k in tqdm(range(1, 11, 1), leave=False, total=10):
+    p = k/200
+    keys = set()
+    for i in tqdm(range(1,len(myList)), leave = False, total=len(myList)):
+        noMetabolites=int(a*i*l)
+        noReactions=int(b*i*l)
+        if i ==1:
+            minNoNodes = noMetabolites+noReactions
+        elif i==len(myList)-1:
+            maxNoNodes = noMetabolites+noReactions
+        for j in tqdm(range(l), leave = False, total=l):
+            G=nx.algorithms.bipartite.random_graph(n=noMetabolites, m=noReactions, p=p, directed=True)
+            X = {n for n,d in G.nodes(data=True) if d['bipartite'] == 0}
+            R = set(G) - X
+            metabolites = set()
+            reactions = set()
+            # assign labels
+            for x in X:
+                G.nodes[x]['Type'] = "Species"
+            for r in R:
+                G.nodes[r]['Type'] = "Reaction"
+            
+            H=deepcopy(G)
+            K=deepcopy(G)
+            L=deepcopy(G)
+
+            timeStamp = time.time()
+            setCounter = sum(1 for c in findAllMRChordlessCyclesSet(H, R, X, bound))
+            setTime = time.time()-timeStamp
+            
+            timeStamp = time.time()
+            listCounter = sum(1 for c in findAllMRChordlessCyclesList(K, R, X, bound))
+            listTime = time.time()-timeStamp
+            
+            if setCounter!=listCounter:
+                print("Set and list counter are not the same", setCounter, listCounter)
+                for c in findAllMRChordlessCyclesSet(deepcopy(G), R, X, bound):
+                    print(c)
+                print()
+                for c in findAllMRChordlessCyclesList(deepcopy(G), R, X, bound):
+                    print(c)
+                input()
+
+            timeStamp = time.time()
+            johnsonCounter = 0
+            for c in nx.simple_cycles(L, length_bound=bound):
+                if checkCycle(c, L):
+                    johnsonCounter+=1
+            if setCounter!=johnsonCounter:                
+                print("Set and Johnson counter are not the same", setCounter, johnsonCounter)
+                print("List and Johnson-counter are", listCounter, johnsonCounter)
+                for c in findAllMRChordlessCyclesList(deepcopy(G), R, X, bound):
+                    print(c)
+                print()
+                for c in nx.simple_cycles(K, length_bound=bound):
+                    if checkCycle(c, K):
+                        print(c)
+                input()
+            johnsonTime = time.time()-timeStamp
+            if "MR-chordlessSet" not in timeDict:
+                timeDict["MR-chordlessSet"]={setCounter: [setTime]}
+            if "MR-chordlessList" not in timeDict:
+                timeDict["MR-chordlessList"]={setCounter: [listTime]}
+            if "Johnson" not in timeDict:
+                timeDict["Johnson"]={setCounter: [johnsonTime]}
+            else:
+                timeDict["MR-chordlessSet"].setdefault(setCounter, []).append(setTime)
+                timeDict["MR-chordlessList"].setdefault(setCounter, []).append(listTime)
+                timeDict["Johnson"].setdefault(setCounter, []).append(johnsonTime)
+            keys.add(setCounter)
+    path = direc+ "/Benchmarking" +str(p) + "min_" + str(minNoNodes) + "max_" +str(maxNoNodes) +".pkl"
+    plotResults(minNoNodes, maxNoNodes, p, sorted(list(keys)), direc, timeDict)
+    with open(path, "wb") as file:
+        pickle.dump(timeDict, file)
